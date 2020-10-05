00f19493c6cf10bf5a434c5d8225e0ca5a3a69c4
Lucas <liger.lucas33@gmail.com>
2020-10-05 16:38:49 +0200

    TP tableau 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,40 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int score[] = {24, 15, 78, 54, 34, 90, 2};
-        int moyenne = 0;
-        int total = score.length;
-
+    static void printScore(int score[]){
         for (int i = 0; i < score.length; i++){
             System.out.println(score[i]);
         }
 
+    }
 
+    static int highScore(int score[]){
+        int highScore = 0;
         for (int i = 0; i < score.length; i++){
-            moyenne = moyenne + score[i];
+            if (score[i]>highScore){
+                highScore = score[i];
+            }
         }
 
-        System.out.println(moyenne/total);
+    return highScore;}
+
+    static boolean isSuperiorTen(int score[]){
+        int a = 10;
+        boolean isSuperior = false;
+        for (int i = 0; i<score.length; i++){
+            if(score[i]>=a){
+                isSuperior = true;
+                System.out.println(score[i] + " est supérieur à 10");
+            }
+        }
+    return isSuperior;}
+
+
+    public static void main(String[] args) {
+        int score[] = {24, 101, 1234, 544, 102, 90, 2};
+
+        printScore(score);
+        System.out.println(highScore(score));
+        isSuperiorTen(score);
     }
 }
