e418ff3134b5c3b64398cf24a21be834c2f4d4d9
Lucas <liger.lucas33@gmail.com>
2020-10-05 16:09:51 +0200

    Premier TP tableau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int score[] = {24, 15, 78, 54, 34, 90, 2};
+        int moyenne = 0;
+        int total = score.length;
+
+        for (int i = 0; i < score.length; i++){
+            System.out.println(score[i]);
+        }
+
+
+        for (int i = 0; i < score.length; i++){
+            moyenne = moyenne + score[i];
+        }
+
+        System.out.println(moyenne/total);
     }
 }
